e40868e58a0ba600d436350be982df775a937349
Petr Iaskevich <iaskdeveloper@gmail.com>
2024-11-16 18:01:38 +0300

    chore: compiler with sanitizers

--- source/main.cpp
+++ source/main.cpp
@@ -1,23 +1,16 @@
-#include <format>
-#include <iostream>
+#include <thread>
+#include <vector>
 
-extern "C"
-{
-#include <libavformat/avformat.h>
-#include <libavformat/version.h>
-}
 
 int main() {
-  std::cout << std::format("avformat version: {}", LIBAVFORMAT_IDENT) << '\n';
-  auto* ctx = avformat_alloc_context();
-  constexpr auto kFilename = std::string_view{"some.mp3"};
-  if (const auto res = avformat_open_input(&ctx, kFilename.data(), nullptr, nullptr); res != 0) {
-    std::cerr << std::format("filed open file with filename: {}", kFilename) << '\n';
-    avformat_free_context(ctx);
-    return -1;
+  unsigned i {};
+  std::vector<std::thread> threads;
+  for (auto idx = 0; idx < 2; ++idx) {
+    threads.emplace_back([&i] mutable noexcept{ ++i; });
   }
-
-  avformat_close_input(&ctx);
-  avformat_free_context(ctx);
+  for (auto& thread : threads) {
+    thread.join();
+    new int{};
+  }
   return 0;
 }
